802a5822cddb69c9902c139e8bf56fe43f515d26
Ssoon-m <tkksm2@gmail.com>
2023-06-14 10:14:08 +0900

    feat: prettier config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,21 @@
+// prettier: prettier
+// eslint-config-prettier: Prettier와 충돌할 수 있는 모든 규칙을 비활성화
+// eslint-plugin-prettier: 코드 포매팅 시 Prettier를 사용하게 만드는 규칙을 추가
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  plugins: ['prettier'],
+  extends: ['next', 'turbo', 'prettier', 'plugin:prettier/recommended'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("next/babel")],
+      presets: [require.resolve('next/babel')],
     },
   },
 };
