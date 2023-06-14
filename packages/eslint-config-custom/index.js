f6d3bef0428870e972b07b1f0950ae4b3f513401
Ssoon-m <tkksm2@gmail.com>
2023-06-14 11:47:01 +0900

    feat: remove eslint-plugin-prettier setting

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,10 +2,10 @@
 // eslint-config-prettier: Prettier와 충돌할 수 있는 모든 규칙을 비활성화
 // eslint-plugin-prettier: 코드 포매팅 시 Prettier를 사용하게 만드는 규칙을 추가
 module.exports = {
-  plugins: ['prettier'],
-  extends: ['next', 'turbo', 'prettier', 'plugin:prettier/recommended'],
+  extends: ['next', 'turbo', 'prettier'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
+    'prettier/prettier': 'off',
   },
   parserOptions: {
     babelOptions: {
