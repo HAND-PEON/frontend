ffa3f359c112e4cead6b5c29518cac975f1f9686
brightchul <abhbtbb1@gmail.com>
2023-06-18 12:09:38 +0900

    feat: add Main page

---
+++ apps/web/src/app/(home)/Main.tsx
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+export default function Main() {
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center">
+      <div className="h-full w-[390px]">
+        <Header />
+      </div>
+    </main>
+  );
+}
