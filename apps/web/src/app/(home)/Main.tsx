1b37f9ecedefec3574e7fb72607f1a8fc5196ce9
brightchul <abhbtbb1@gmail.com>
2023-06-18 22:44:51 +0900

    feat: add Banner slides

--- apps/web/src/app/(home)/Main.tsx
+++ apps/web/src/app/(home)/Main.tsx
@@ -1,10 +1,24 @@
+import BannerSlides, { BannerInfo } from '@/components/BannerSlides';
+
 import Header from './Header';
+
+const bannerData: BannerInfo[] = [
+  { src: '/image/banner1.png', url: '' },
+  { src: '/image/banner2.png', url: '' },
+  { src: '/image/banner3.png', url: '' },
+  { src: '/image/banner4.png', url: '' },
+  { src: '/image/banner5.png', url: '' },
+  { src: '/image/banner6.png', url: '' },
+];
 
 export default function Main() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center">
       <div className="h-full w-[390px]">
         <Header />
+        <div>
+          <BannerSlides data={bannerData} totalViewURL="/recommend" />
+        </div>
       </div>
     </main>
   );
---
+++ apps/web/src/components/BannerSlides/index.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import 'swiper/css';
+import 'swiper/css/autoplay';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import TotalViewButton from './TotalViewButton';
+
+export type BannerInfo = {
+  src: string;
+  url: string;
+};
+
+interface BannerSlidesProps {
+  data: BannerInfo[];
+  totalViewURL?: string;
+}
+
+export default function BannerSlides({
+  data,
+  totalViewURL,
+}: BannerSlidesProps) {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  return (
+    <div className="relative h-[210px] overflow-hidden">
+      <TotalViewButton
+        totalViewURL={totalViewURL}
+        currentSlideIndex={slideIndex + 1}
+        totalLength={data.length}
+        totalViewText="모두 보기"
+      />
+      <Swiper
+        modules={[Autoplay, A11y]}
+        autoplay
+        loop
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        onSlideChange={({ realIndex }) => {
+          setSlideIndex(realIndex);
+        }}
+      >
+        {data.map(({ src, url }, idx) => (
+          <SwiperSlide key={`${idx}-${src}`}>
+            <Link href={url}>
+              <Image
+                className="!relative !object-cover"
+                alt={src}
+                src={src}
+                fill
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
