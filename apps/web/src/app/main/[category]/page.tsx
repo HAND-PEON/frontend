6e1b6c74602e4b33f96415a8b448d664aa8ed3e7
brightchul <abhbtbb1@gmail.com>
2023-06-23 07:01:03 +0900

    feat: change (home) to main path

--- apps/web/src/app/main/[category]/page.tsx
+++ apps/web/src/app/main/[category]/page.tsx
@@ -14,7 +14,7 @@ interface CategoryPageProps {
 const conveniences: Convenience[] = ['ALL', 'CU', 'GS25', '7Eleven', 'Emart24'];
 const categoryInfoList = conveniences.map((convenience) => ({
   label: convenience,
-  href: `/${convenience}`,
+  href: `/main/${convenience}`,
 }));
 
 export default function CategoryPage({
