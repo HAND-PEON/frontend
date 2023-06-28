89dccc61a2b3bf559dbc9a2ae83ac59a1ab544d3
brightchul <abhbtbb1@gmail.com>
2023-06-28 22:56:03 +0900

    style: add main-hottrend area styling

--- apps/web/src/app/main/[category]/HotTrend.tsx
+++ apps/web/src/app/main/[category]/HotTrend.tsx
@@ -1,3 +1,5 @@
+import ChevronIcon from '@/components/icons/ChevronIcon';
+
 import { Convenience } from '../../type';
 
 interface HotTrendProps {
@@ -5,5 +7,19 @@ interface HotTrendProps {
 }
 
 export default function HotTrend({ convenience }: HotTrendProps) {
-  return <div className="bg-white">{convenience} 이번주 핫트렌드</div>;
+  return (
+    <div className="rounded-t-[30px] bg-white px-5 pb-10 pt-[27px]">
+      <div className="flex flex-wrap items-start justify-start gap-y-5">
+        핫트렌드 영역
+      </div>
+      <div className="mt-10">
+        <button className="w-full rounded-[7px] bg-[#1E1C1C] py-3 text-white">
+          <span className="flex items-center justify-center gap-1">
+            더보기
+            <ChevronIcon />
+          </span>
+        </button>
+      </div>
+    </div>
+  );
 }
