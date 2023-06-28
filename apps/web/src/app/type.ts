1532cdf117fea6cb8971f0a179bcee997715baf8
Ssoon-m <tkksm2@gmail.com>
2023-06-28 14:40:34 +0900

    fix: type변경 및 코드 스타일 수정

--- apps/web/src/app/type.ts
+++ apps/web/src/app/type.ts
@@ -1,8 +1,1 @@
 export type Convenience = 'ALL' | 'CU' | 'GS25' | '7Eleven' | 'Emart24';
-
-/**
- * 숫자를 천단위 마다 콤마로 변환
- */
-export const formatNumberWithComma = (n: number) => {
-  return n.toLocaleString('ko-KR');
-};
--- apps/web/src/components/EventItemCard/index.tsx
+++ apps/web/src/components/EventItemCard/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
 import { Convenience } from '@/app/type';
-import { formatNumberWithComma } from '@/app/type';
+import { formatNumberWithComma } from '@/utils/number-formatter';
 
 interface EventItem {
-  eventType: 'BOGO' | 'BTGO' | 'SALE' | 'BONUS';
+  eventType: 'ONE_PLUS_ONE' | 'TWO_PLUS_ONE' | 'SALE' | 'BONUS';
   imageUrl: string;
   price: number;
   title: string;
-  Convenience: Convenience;
+  convenience: 'ALL' | 'CU' | 'GS25' | '7Eleven' | 'Emart24';
 }
 
 const EventTypeTag: Record<
   EventItem['eventType'],
   { text: string; bg: string }
 > = {
-  BOGO: { text: '1+1', bg: 'bg-[#73F69D]' },
-  BTGO: { text: '2+1', bg: 'bg-[#E2F981]' },
+  ONE_PLUS_ONE: { text: '1+1', bg: 'bg-[#73F69D]' },
+  TWO_PLUS_ONE: { text: '2+1', bg: 'bg-[#E2F981]' },
   SALE: { text: '할인', bg: 'bg-[#FFA8A5]' },
   BONUS: { text: '+덤', bg: 'bg-[#C29DF6]' },
 };
@@ -25,25 +25,25 @@ interface Props {
   EventItem: EventItem;
 }
 
-const EventItemCard = ({ EventItem }: Props) => {
+const EventItemCard = ({
+  EventItem: { eventType, imageUrl, convenience, title, price },
+}: Props) => {
   return (
     <div className="w-[calc(50%_-_9px)]">
       <div className="mb-[12px] h-[164px] w-full rounded-[9px] border-2 border-b-[4px] border-r-[4px] border-black p-[5px]">
         <div className="relative flex h-full w-full items-center justify-center">
           <div
-            className={`absolute left-0 top-0 z-10 flex h-[24px] w-min items-center justify-center whitespace-nowrap rounded-[20px] border border-black px-[10px] ${
-              EventTypeTag[EventItem.eventType].bg
-            }`}
+            className={`absolute left-0 top-0 z-10 flex h-[24px] w-min items-center justify-center whitespace-nowrap rounded-[20px] border border-black px-[10px] ${EventTypeTag[eventType].bg}`}
           >
             <span className="text-xs font-bold">
-              {EventTypeTag[EventItem.eventType].text}
+              {EventTypeTag[eventType].text}
             </span>
           </div>
           <div className="h-full w-full rounded-[3px]">
-            {EventItem.imageUrl && (
+            {imageUrl && (
               <Image
                 className="rounded-[3px] object-contain"
-                src={EventItem.imageUrl}
+                src={imageUrl}
                 alt="pyeon-event-image"
                 fill
               />
@@ -52,15 +52,15 @@ const EventItemCard = ({ EventItem }: Props) => {
         </div>
       </div>
       <div className="mb-[8px] flex w-min items-center justify-center rounded-[4px] border border-black bg-white px-[7px] py-[2px]">
-        <span className="text-xs font-bold">{EventItem.Convenience}</span>
+        <span className="text-xs font-bold">{convenience}</span>
       </div>
       <div className="leading-5">
         <div className="truncate">
-          <span>{EventItem.title}</span>
+          <span>{title}</span>
         </div>
         <div>
           <span className="font-bold">
-            {`${formatNumberWithComma(EventItem.price)}원`}
+            {`${formatNumberWithComma(price)}원`}
           </span>
         </div>
       </div>
