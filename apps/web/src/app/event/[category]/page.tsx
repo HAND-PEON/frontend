ee10ae2ddcb7d4bdd479b404f812759259924cb8
순민 <tkksm2@gmail.com>
2023-06-30 00:06:12 +0900

    feat: change icon

--- apps/web/src/app/event/[category]/page.tsx
+++ apps/web/src/app/event/[category]/page.tsx
@@ -28,29 +28,31 @@ const EventCategoryPage = ({
           categoryData={categoryInfoList}
           currentCategory={category}
         />
-        <div className="pb-[18px] pt-[40px]">
-          <span className="text-[1.375rem] font-bold">이번주 행사 품목</span>
-        </div>
-        <div className="pb-[29px]">
-          <Chip>
-            {eventCategory.map((item, index) => (
-              <Chip.Item myIndex={index} key={index} text={item} />
+        <div className="px-[20px]">
+          <div className="pb-[18px] pt-[40px]">
+            <span className="text-[1.375rem] font-bold">이번주 행사 품목</span>
+          </div>
+          <div className="pb-[29px]">
+            <Chip>
+              {eventCategory.map((item, index) => (
+                <Chip.Item myIndex={index} key={index} text={item} />
+              ))}
+            </Chip>
+          </div>
+          <div className="flex flex-wrap items-start justify-start gap-x-[18px] gap-y-[50px]">
+            {Array.from({ length: 8 }).map((_, i) => (
+              <EventItemCard
+                key={i}
+                eventItem={{
+                  eventType: 'ONE_PLUS_ONE',
+                  imageUrl: pyeonImage,
+                  price: 20000,
+                  title: 'asdfasdf',
+                  convenience: '7Eleven',
+                }}
+              />
             ))}
-          </Chip>
-        </div>
-        <div className="flex flex-wrap items-start justify-start gap-x-[18px] gap-y-[50px]">
-          {Array.from({ length: 8 }).map((_, i) => (
-            <EventItemCard
-              key={i}
-              eventItem={{
-                eventType: 'ONE_PLUS_ONE',
-                imageUrl: pyeonImage,
-                price: 20000,
-                title: 'asdfasdf',
-                convenience: '7Eleven',
-              }}
-            />
-          ))}
+          </div>
         </div>
         <div className="fixed bottom-7 right-6 z-50 ml-auto mr-auto">
           <TopButton />
