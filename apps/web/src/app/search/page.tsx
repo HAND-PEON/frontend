20c49335268e6bda8418e04101f34151b6bf6285
순민 <tkksm2@gmail.com>
2023-07-02 23:48:04 +0900

    feat: search init page

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -1,13 +1,17 @@
 import BasicLayout from '@/components/BasicLayout';
 import HotTrendSection from './components/HotTrendSection';
 import React from 'react';
+import PerfectMatchSection from './components/PerfectMatchSection';
 
 const SearchPage = () => {
   return (
     <BasicLayout isSearchHeader>
-      <div className="flex-1 bg-white ">
-        <div className="px-[20px] pt-[37px]">
+      <div className="flex-1 bg-white pb-[134px]">
+        <div className="px-[20px] pb-[52px] pt-[37px]">
           <HotTrendSection />
+        </div>
+        <div className="pl-[20px]">
+          <PerfectMatchSection />
         </div>
       </div>
     </BasicLayout>
