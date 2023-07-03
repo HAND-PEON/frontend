ef32ae491d978848c14ecfe15a738987ecc1aa56
brightchul <abhbtbb1@gmail.com>
2023-07-03 14:14:10 +0900

    feat: modify case logic

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -4,12 +4,12 @@ import Error from 'next/error';
 import { redirect, usePathname } from 'next/navigation';
 
 export default function NotFound() {
-  const path = usePathname();
+  const segmentList = usePathname().split('/');
 
-  switch (path) {
-    case '/hottrend':
-      return redirect('/hottrend/CU');
-    case '/event':
+  switch (true) {
+    case segmentList.includes('hottrend'):
+      return redirect('/hottrend/CU/1');
+    case segmentList.includes('event'):
       return redirect('/event/ALL');
     default:
       return <Error statusCode={404} />;
