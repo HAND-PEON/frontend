cdf5eb363d28384135cb903a911cfbbaadf20756
brightchul <abhbtbb1@gmail.com>
2023-07-03 13:27:59 +0900

    feat: add Link to HotTrendCard

--- apps/web/src/components/HotTrendItem/index.tsx
+++ apps/web/src/components/HotTrendItem/index.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { Convenience } from '@/app/type';
 import RankBox from '@/components/RankBox';
-import { pyeonImage } from '@/dummy/image';
 import { formatNumberWithComma } from '@/utils/numberFormatter';
 
 interface HotTrendCardProps {
@@ -17,42 +17,44 @@ interface HotTrendCardProps {
 export default function HotTrendCard({
   id,
   rank,
-  imageUrl = pyeonImage,
+  imageUrl,
   price,
   title,
   convenience,
 }: HotTrendCardProps) {
   return (
     <div className="min-h-[165px]  w-full">
-      <div className="relative flex  h-[165px] rounded-[9px] border-2 border-b-[3px] border-r-[7px] border-[#1E1C1C] p-[10px]">
-        <div className="relative my-2 ml-[27px] flex-1">
-          <Image
-            className="object-contain"
-            src={imageUrl}
-            fill
-            alt={`hottrend-${title}`}
-          />
-        </div>
-        <div className="mt-[46px] w-[168px]">
-          <div>
-            <span className="rounded-sm border-[1px] border-[#1E1C1C] px-[7px] py-[2px] font-bold leading-none">
-              {convenience}
-            </span>
+      <Link href={`/hottrend/${convenience}/${rank}`}>
+        <div className="relative flex  h-[165px] rounded-[9px] border-2 border-b-[3px] border-r-[7px] border-[#1E1C1C] p-[10px]">
+          <div className="relative my-2 ml-[27px] flex-1">
+            <Image
+              className="object-contain"
+              src={imageUrl}
+              fill
+              alt={`hottrend-${title}`}
+            />
           </div>
-          <div className="mt-[11px] truncate text-base font-medium	leading-none">
-            {title}
+          <div className="mt-[46px] w-[168px]">
+            <div>
+              <span className="rounded-sm border-[1px] border-[#1E1C1C] px-[7px] py-[2px] font-bold leading-none">
+                {convenience}
+              </span>
+            </div>
+            <div className="mt-[11px] truncate text-base font-medium	leading-none">
+              {title}
+            </div>
+            <div className="mt-[2px] leading-none">
+              <span className="text-[22px] font-bold ">
+                {formatNumberWithComma(price)}
+              </span>
+              <span>원</span>
+            </div>
           </div>
-          <div className="mt-[2px] leading-none">
-            <span className="text-[22px] font-bold ">
-              {formatNumberWithComma(price)}
-            </span>
-            <span>원</span>
+          <div className="absolute">
+            <RankBox rank={rank} />
           </div>
         </div>
-        <div className="absolute">
-          <RankBox rank={rank} />
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
