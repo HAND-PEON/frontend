fc5288d304e9902623282b598269b2f156334ac4
순민 <tkksm2@gmail.com>
2023-07-05 22:40:53 +0900

    feat: search result page ui

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -1,19 +1,11 @@
 import BasicLayout from '@/components/BasicLayout';
-import HotTrendSection from './components/HotTrendSection';
 import React from 'react';
-import PerfectMatchSection from './components/PerfectMatchSection';
+import SearchRenderer from './SearchRenderer';
 
 const SearchPage = () => {
   return (
     <BasicLayout hasSearchHeader>
-      <div className="flex-1 bg-white pb-[134px]">
-        <div className="px-[20px] pb-[52px] pt-[37px]">
-          <HotTrendSection />
-        </div>
-        <div className="pl-[20px]">
-          <PerfectMatchSection />
-        </div>
-      </div>
+      <SearchRenderer />
     </BasicLayout>
   );
 };
