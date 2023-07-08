8b57e4a958566b30caef1fbec638878b9a8e4375
순민 <tkksm2@gmail.com>
2023-07-08 22:31:55 +0900

    feat: change text

--- apps/web/src/app/search/view/SearchView.tsx
+++ apps/web/src/app/search/view/SearchView.tsx
@@ -4,7 +4,7 @@ import PerfectMatchSection from '../components/PerfectMatchSection';
 
 const SearchView = () => {
   return (
-    <div className="flex-1 bg-white pb-[134px]">
+    <div className="flex-1 bg-white pb-[20px]">
       <div className="px-[20px] pb-[52px] pt-[37px]">
         <HotTrendSection />
       </div>
--- apps/web/src/components/SearchLayout/index.tsx
+++ apps/web/src/components/SearchLayout/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import BasicHeader from '../BasicHeader';
 import SearchHeader from '../SearchHeader';
 import useScrollDriectionDetect from '@/hooks/useScrollDriectionDetect';
+import HandpyeonTextIcon from '../icons/HandpyeonTextIcon';
 
 interface Props extends React.ComponentProps<typeof BasicHeader> {
   children: React.ReactNode;
@@ -13,17 +14,17 @@ const SearchLayout = ({ children }: Props) => {
   return (
     <>
       <div className="flex h-[46px] w-full items-center justify-center bg-white">
-        <span className="text-base font-bold text-[#1E1C1C]">
-          오늘 1+1 행사 상품이 궁금할 땐,{' '}
-          <span className="text-[1.36856rem] font-bold text-[#1E1C1C]">
-            핸드편
+        <div className="flex items-center justify-center gap-2">
+          <span className="text-base font-bold text-[#1E1C1C]">
+            오늘 1+1 행사 상품이 궁금할 땐,
           </span>
-        </span>
+          <HandpyeonTextIcon />
+        </div>
       </div>
       <div className={`${isScrollDown ? 'relative' : 'sticky top-0 z-30'}`}>
         <SearchHeader />
       </div>
-      <div className="flex flex-1 flex-col bg-[#F7F7F7]">{children}</div>
+      <div className="bg-[#F7F7F7]] flex flex-1 flex-col">{children}</div>
     </>
   );
 };
