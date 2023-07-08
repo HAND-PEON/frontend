e417e77ce32a3bf0232f8b97b61347732d22dadf
brightchul <abhbtbb1@gmail.com>
2023-07-08 16:07:38 +0900

    feat: add props ContentItem

--- apps/web/src/app/recommendation/[category]/components/ContentItem.tsx
+++ apps/web/src/app/recommendation/[category]/components/ContentItem.tsx
@@ -3,17 +3,25 @@ import Image from 'next/image';
 
 interface ContentItemProps {}
 
-export default function ContentItem(props: ContentItemProps) {
+interface ContentItemProps {
+  imageURL: string;
+  subTitle: string;
+  mainTitle: string;
+}
+
+export default function ContentItem({
+  imageURL,
+  subTitle,
+  mainTitle,
+}: ContentItemProps) {
   return (
     <div className="border-main1 border-2">
       <div className="min-h-[122px] bg-[#73F69D]">
-        <Image src={''} alt={''} />
+        <Image src={imageURL} alt={''} />
       </div>
       <div className="px-10px py-15px">
-        <div className="text-main1 text-[12px] font-medium">
-          집에서 분위기 낼 수 있는
-        </div>
-        <div className="text-main1 text-[15px] font-bold">하이볼 레시피</div>
+        <div className="text-main1 text-[12px] font-medium">{subTitle}</div>
+        <div className="text-main1 text-[15px] font-bold">{mainTitle}</div>
       </div>
     </div>
   );
