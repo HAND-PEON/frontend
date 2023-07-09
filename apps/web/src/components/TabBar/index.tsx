8cc2a1ff612ebbba666f41d73e92dd8ea886691c
brightchul <abhbtbb1@gmail.com>
2023-07-09 17:14:28 +0900

    fix: add emoji to TabBar's active text

--- apps/web/src/components/TabBar/index.tsx
+++ apps/web/src/components/TabBar/index.tsx
@@ -11,10 +11,12 @@ export default function TabBar({ currentIndex, onClick }: TabBarProps) {
   return (
     <div className="p-5px flex rounded-full bg-white">
       <OneBar isActive={0 === currentIndex} onClick={() => onClick(0)}>
-        이번주 Hot Trend
+        {0 === currentIndex && '이번주 🔥Hot Trend'}
+        {0 !== currentIndex && '이번주 Hot Trend'}
       </OneBar>
       <OneBar isActive={1 === currentIndex} onClick={() => onClick(1)}>
-        이번주 ⚡행사상품⚡️
+        {1 === currentIndex && '이번주 ⚡행사상품⚡️'}
+        {1 !== currentIndex && '이번주 행사상품'}
       </OneBar>
     </div>
   );
