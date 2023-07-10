b959853126651a5e2174a4710fbb6dffabb53648
brightchul <abhbtbb1@gmail.com>
2023-07-10 18:21:56 +0900

    feat: add confitional headerText
    - /recommendation/[category]/ , /recommendation/contents/[id]
    
    각 route에 대해서 layout header  글자가 변경되어야 해서 수정합니다.

--- apps/web/src/app/recommendation/layout.tsx
+++ apps/web/src/app/recommendation/layout.tsx
@@ -1,16 +1,27 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
 import BasicLayout from '@/components/BasicLayout';
 import HomeIconButton from '@/components/HomeIconButton';
 import SearchIconButton from '@/components/SearchIconButton';
 
-interface HotTrendLayoutProps {
+interface RecommendationLayoutProps {
   children: React.ReactNode;
 }
 
-export default function HotTrendLayout({ children }: HotTrendLayoutProps) {
+export default function RecommendationLayout({
+  children,
+}: RecommendationLayoutProps) {
+  const pathname = usePathname();
+  const headerText = pathname.includes('/recommendation/contents')
+    ? ''
+    : '편슐랭 가이드';
+
   return (
     <BasicLayout
       hasBackButton
-      headerCenter={'편슐랭 가이드'}
+      headerCenter={headerText}
       headerRight={
         <div className="flex items-center">
           <HomeIconButton />
@@ -18,7 +29,7 @@ export default function HotTrendLayout({ children }: HotTrendLayoutProps) {
         </div>
       }
     >
-      <div className="h-full bg-white">{children}</div>
+      <div className="h-full">{children}</div>
     </BasicLayout>
   );
 }
