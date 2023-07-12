402c4966af0cdbb57920706b37437baf629cf1df
Ssoon-m <tkksm2@gmail.com>
2023-07-12 09:53:52 +0900

    feat: add http client

--- apps/web/src/app/main/[category]/page.tsx
+++ apps/web/src/app/main/[category]/page.tsx
@@ -7,6 +7,7 @@ import CategoryChildren from '../CategoryChildren';
 import EventItems from './EventItems';
 import HotTrend from './HotTrend';
 import { CONVENIENCE } from '@/constants/conveniences';
+import httpClient from '@/http/client';
 
 interface CategoryPageProps {
   params: { category: Convenience };
---
+++ apps/web/src/http/client.ts
@@ -0,0 +1,114 @@
+import qs from 'qs';
+
+interface RequestConfig {
+  params?: any;
+  headers?: HeadersInit;
+  signal?: AbortSignal;
+}
+
+export interface HttpClient {
+  baseUrl?: string;
+  get?<T>(
+    url: string,
+    config?: RequestConfig,
+  ): Promise<{ data: T; headers?: Headers }>;
+  post?<T>(
+    url: string,
+    body?: any,
+    config?: RequestConfig,
+  ): Promise<{ data: T; headers?: Headers }>;
+  patch?<T>(
+    url: string,
+    body?: any,
+    config?: RequestConfig,
+  ): Promise<{ data: T; headers?: Headers }>;
+  delete?<T>(
+    url: string,
+    config?: RequestConfig,
+  ): Promise<{ data: T; headers?: Headers }>;
+}
+
+const httpClient: HttpClient = {
+  baseUrl: 'http://api.handpyeon.com',
+  async get<T>(url: string, config: RequestConfig = {}) {
+    const queryString = config?.params
+      ? qs.stringify(config.params, { addQueryPrefix: true })
+      : '';
+
+    // baseAPI()
+    const res = await fetch(this.baseUrl.concat(url, queryString), {
+      method: 'GET',
+      headers: {
+        ...(config?.headers ?? {}),
+      },
+    });
+    const data: T = await res.json();
+
+    const { ok, headers } = res;
+    if (ok) {
+      return { data, headers };
+    }
+    return Promise.reject(data);
+  },
+  async post<T>(url: string, body?: any, config: RequestConfig = {}) {
+    const res = await fetch(this.baseUrl.concat(url), {
+      method: 'POST',
+      headers: {
+        ...(body ? { 'Content-Type': 'application/json' } : {}),
+        ...(config.headers ?? {}),
+      },
+      signal: config.signal,
+      body: body ? JSON.stringify(body) : undefined,
+    });
+    const data: T = await res.json();
+
+    const { ok, headers } = res;
+    if (ok) {
+      return { data, headers };
+    }
+    return Promise.reject(data);
+  },
+  async patch<T>(url: string, body: any, config: RequestConfig = {}) {
+    const res = await fetch(this.baseUrl.concat(url), {
+      method: 'PATCH',
+      ...(typeof window === 'undefined' ? {} : { credentials: 'include' }),
+      headers: {
+        ...(body ? { 'Content-Type': 'application/json' } : {}),
+        ...(config.headers ?? {}),
+      },
+      signal: config.signal,
+      body: JSON.stringify(body),
+    });
+
+    const data: T = await res.json();
+
+    const { ok, headers } = res;
+    if (ok) {
+      return { data, headers };
+    }
+    return Promise.reject(data);
+  },
+  async delete<T>(url: string, config: RequestConfig = {}) {
+    const querystring = config?.params
+      ? qs.stringify(config?.params, { addQueryPrefix: true })
+      : '';
+
+    const res = await fetch(this.baseUrl.concat(url, querystring), {
+      method: 'DELETE',
+      headers: {
+        ...(config.headers ?? {}),
+      },
+      signal: config.signal,
+    });
+
+    const data: T = await res.json();
+
+    const { ok, headers } = res;
+    if (ok) {
+      return { data, headers };
+    }
+    return Promise.reject(data);
+  },
+};
+
+export default httpClient;
