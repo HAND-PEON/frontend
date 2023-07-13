6504bf55ada615f8a840908aea9c37493eb2a4bc
순민 <tkksm2@gmail.com>
2023-07-13 23:38:13 +0900

    fix: image reduce

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -21,50 +21,50 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" className="h-full">
-      <body className={`${pretendard.className} h-full bg-[#FEF9E5]`}>
+      <body className={`${pretendard.className} h-ful bg-[#FEF9E5]`}>
         <aside className="fixed left-1/2 z-0 ml-[_-500px] hidden h-full w-[423px] lg:flex lg:items-center">
           <div>
             <div>
               <Image
                 src="/image/intro-contents.png"
                 alt="intro-contents"
-                width={423}
-                height={232}
+                width={345}
+                height={190}
                 priority
               />
             </div>
             {/* spacing block */}
-            <div className="h-[106px]"></div>
+            <div className="h-[87px]"></div>
             <div className="relative">
               <Image
                 src="/image/handpyeon2.png"
                 alt="handpyeon-text"
-                width={358}
-                height={138}
+                width={300}
+                height={113}
                 priority
               />
               <div className="absolute right-[_-80px] top-[_-65px] h-[84px] w-[84px]">
                 <Image
                   src="/image/LeftArrow.png"
                   alt="left-arrow"
-                  width={84}
-                  height={84}
+                  width={68}
+                  height={68}
                   priority
                 />
               </div>
-              <div className="absolute bottom-[_-150px] left-[_-80px] h-[84px] w-[84px]">
+              <div className="absolute bottom-[_-130px] left-[_-80px] h-[84px] w-[84px]">
                 <Image
                   src="/image/RightArrow.png"
                   alt="right-arrow"
-                  width={84}
-                  height={84}
+                  width={68}
+                  height={68}
                   priority
                 />
               </div>
             </div>
           </div>
         </aside>
-        <div className="fixed  bottom-0 right-0">
+        <div className="fixed  bottom-0 right-0 hidden lg:block">
           <Image
             src="/image/brand-contents.png"
             alt="brand-contents"
@@ -74,7 +74,7 @@ export default function RootLayout({
           />
         </div>
         <main className="flex min-h-full w-full justify-center lg:justify-normal">
-          <div className="z-50 flex w-full min-w-[360px] max-w-[390px] flex-col border-[1px] border-[#1E1C1C] lg:ml-[59%]">
+          <div className="z-50 flex w-full min-w-[360px] max-w-[390px] flex-col border-x-[1px] border-[#1E1C1C] lg:ml-[55%]">
             <AppProviders>{children}</AppProviders>
           </div>
         </main>
