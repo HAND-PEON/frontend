6c3e9ca208a9915de4dc90da75b52abc40d65901
hoon <57323359+brightchul@users.noreply.github.com>
2023-07-15 18:34:42 +0900

    feat(web): 핫트렌드 API 연동 (#30)
    * fix: fix the capital word
    
    * feat: add idx to EventItemCard's key
    
    * feat: add hotTrendGoods api and hooks
    
    * feat: add HotTrendGoods types
    
    * feat: apply useGetHotTrendGoods to main HotTrend
    
    * feat: apply api to main banner
    
    * refactor: remove unused code
    
    * feat: add Link component

--- apps/web/src/app/main/[category]/HotTrend.tsx
+++ apps/web/src/app/main/[category]/HotTrend.tsx
@@ -1,28 +1,42 @@
+import Link from 'next/link';
+
+import { HotTrendCategory } from '@/apis/type';
 import HotTrendCard from '@/components/HotTrendItem';
 import ChevronIcon from '@/components/icons/ChevronIcon';
-
-import { Convenience } from '@/app/type';
-import { mainHotTrendData } from '@/dummy/hotTrend';
+import { useGetHotTrendGoods } from '@/hooks/query/useHotTrends';
 
 interface HotTrendProps {
-  convenience: Convenience;
+  convenience: HotTrendCategory;
 }
 
+const mappingSegments: Record<HotTrendCategory, string> = {
+  ALL: 'CU',
+  CU: 'CU',
+  GS25: 'GS25',
+  '7ELEVEN': '7ELEVEN',
+  EMART24: 'EMART24',
+};
+
 export default function HotTrend({ convenience }: HotTrendProps) {
+  const { data } = useGetHotTrendGoods(convenience);
+
   return (
     <div>
       <div className="flex flex-wrap items-start justify-start gap-y-5">
-        {mainHotTrendData.map((props) => (
-          <HotTrendCard key={props.id} {...props} />
+        {data?.map((props, idx) => (
+          <HotTrendCard key={`${idx}-${props.goodsName}`} {...props} />
         ))}
       </div>
       <div className="mt-10">
-        <button className="w-full rounded-[7px] bg-[#1E1C1C] py-3 text-white">
+        <Link
+          href={`/hottrend/${mappingSegments[convenience]}/1`}
+          className="block w-full rounded-[7px] bg-[#1E1C1C] py-3 text-white"
+        >
           <span className="flex items-center justify-center gap-1">
             더보기
             <ChevronIcon />
           </span>
-        </button>
+        </Link>
       </div>
     </div>
   );
