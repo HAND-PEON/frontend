1c04e4e27b02dc7249ae46054b1f30edc974b9f9
brightchul <abhbtbb1@gmail.com>
2023-07-15 17:15:11 +0900

    feat: add Link component

--- apps/web/src/app/main/[category]/HotTrend.tsx
+++ apps/web/src/app/main/[category]/HotTrend.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { HotTrendCategory } from '@/apis/type';
 import HotTrendCard from '@/components/HotTrendItem';
 import ChevronIcon from '@/components/icons/ChevronIcon';
@@ -6,6 +8,14 @@ import { useGetHotTrendGoods } from '@/hooks/query/useHotTrends';
 interface HotTrendProps {
   convenience: HotTrendCategory;
 }
+
+const mappingSegments: Record<HotTrendCategory, string> = {
+  ALL: 'CU',
+  CU: 'CU',
+  GS25: 'GS25',
+  '7ELEVEN': '7ELEVEN',
+  EMART24: 'EMART24',
+};
 
 export default function HotTrend({ convenience }: HotTrendProps) {
   const { data } = useGetHotTrendGoods(convenience);
@@ -18,12 +28,15 @@ export default function HotTrend({ convenience }: HotTrendProps) {
         ))}
       </div>
       <div className="mt-10">
-        <button className="w-full rounded-[7px] bg-[#1E1C1C] py-3 text-white">
+        <Link
+          href={`/hottrend/${mappingSegments[convenience]}/1`}
+          className="block w-full rounded-[7px] bg-[#1E1C1C] py-3 text-white"
+        >
           <span className="flex items-center justify-center gap-1">
             더보기
             <ChevronIcon />
           </span>
-        </button>
+        </Link>
       </div>
     </div>
   );
