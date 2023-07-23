41d384377e870e276f92b6b138ecb2581f727839
hoon <57323359+brightchul@users.noreply.github.com>
2023-07-23 23:05:43 +0900

    feat(web): 추천 콘텐츠 배너 API 업데이트  (#36)
    * feat: update api interfaces
    
    * feat: update recommendation banner API

--- apps/web/src/apis/recommendation.ts
+++ apps/web/src/apis/recommendation.ts
@@ -2,18 +2,33 @@ import { RecommendationCategory } from '@/app/type';
 import httpClient from '@/http/client';
 
 import {
+  PageInfo,
   RecommendationBanner,
   RecommendationDetail,
   RecommendationSummary,
 } from './type';
 
+const TRESH_TEXTS = ['http://~', 'https://~', '://~'];
+const DEFAULT_BANNER_IMAGE = '/image/banner1.png';
+
+function checkTreshURL(url: string) {
+  if (TRESH_TEXTS.some((text) => url.includes(text)))
+    return DEFAULT_BANNER_IMAGE;
+  return url;
+}
+
+interface RecommendationBannersResponse {
+  pageInfo: PageInfo | null;
+  data: RecommendationBanner[];
+}
+
 export const getRecommendationBanners = async () => {
-  const { data } = await httpClient.get<{ data: RecommendationBanner[] }>(
+  const { data } = await httpClient.get<RecommendationBannersResponse>(
     '/handpyeon/api/banners/recommend',
   );
 
-  return data.data.map(({ bannerImage, contentsNumber }) => ({
-    src: bannerImage,
+  return data.data.map(({ bannerImageUrl, contentsNumber }) => ({
+    src: checkTreshURL(bannerImageUrl),
     url: `/recommendation/contents/${contentsNumber}`,
   }));
 };
