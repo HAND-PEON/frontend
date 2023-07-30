7535a135567803cb6a0f8ed4382a1e8599828347
순민 <tkksm2@gmail.com>
2023-07-30 19:24:13 +0900

    feat: connect api to search main HotTrendSection

--- apps/web/src/app/search/components/HotTrendSection.tsx
+++ apps/web/src/app/search/components/HotTrendSection.tsx
@@ -1,27 +1,31 @@
+'use client';
 import React from 'react';
 import { useGetHotTrendGoods } from '@/hooks/query/useHotTrends';
 import { getRandomFromArray } from '@/utils/getRandomFromArray';
-import { CONVENIENCE } from '@/constants/conveniences';
+import { CONVENIENCE, HotTrendMapping } from '@/constants/conveniences';
 
 const HotTrendSection = () => {
-  const a = getRandomFromArray(CONVENIENCE);
-
+  const category = getRandomFromArray(CONVENIENCE);
+  const { data } = useGetHotTrendGoods(HotTrendMapping[category]);
+  console.log('data', data);
   return (
     <div>
       <p className="text-[1.375rem]">
         <span>
           이번주 [{' '}
-          <span className="px-1 shadow-[inset_0_-0.8rem_0_0_#E2F982]">CU</span>{' '}
+          <span className="px-1 shadow-[inset_0_-0.8rem_0_0_#E2F982]">
+            {category}
+          </span>{' '}
           ]
         </span>
         <span className="font-bold">&nbsp;Hot Trend</span>🔥
       </p>
       <ul className="flex flex-col gap-[22px] pt-[25px]">
-        {dummy.map((item, i) => (
+        {data.slice(0, 5).map((item, i) => (
           <li className={i < 3 && `font-bold`} key={i}>
             <div className="flex justify-start gap-5">
               <span>{i + 1}</span>
-              <span className="w-full truncate">{item}</span>
+              <span className="w-full truncate">{item.goodsName}</span>
             </div>
           </li>
         ))}
--- apps/web/src/app/search/view/SearchView.tsx
+++ apps/web/src/app/search/view/SearchView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import HotTrendSection from '../components/HotTrendSection';
 import PerfectMatchSection from '../components/PerfectMatchSection';
 
@@ -6,7 +6,9 @@ const SearchView = () => {
   return (
     <div className="flex-1 bg-white pb-[20px]">
       <div className="px-[20px] pb-[52px] pt-[37px]">
-        <HotTrendSection />
+        <Suspense>
+          <HotTrendSection />
+        </Suspense>
       </div>
       <div className="pl-[20px]">
         <PerfectMatchSection />
