58551bad408b98d76dfa55109affef73b9fefd15
brightchul <abhbtbb1@gmail.com>
2023-08-01 19:24:12 +0900

    feat: modify props in error component

--- apps/web/src/app/recommendation/contents/[id]/error.tsx
+++ apps/web/src/app/recommendation/contents/[id]/error.tsx
@@ -3,5 +3,11 @@
 import ErrorSorry from '@/components/ErrorSorry';
 
 export default function Error() {
-  return <ErrorSorry mainText="요청한 컨텐츠를 찾을 수 없어요." />;
+  return (
+    <ErrorSorry
+      redirectUrl="/main/ALL"
+      buttonText="홈으로 돌아가기"
+      mainText="요청한 컨텐츠를 찾을 수 없어요."
+    />
+  );
 }
--- apps/web/src/components/ErrorSorry/index.tsx
+++ apps/web/src/components/ErrorSorry/index.tsx
@@ -5,30 +5,34 @@ import Link from 'next/link';
 
 interface ErrorProps {
   redirectUrl?: string;
-  mainText?: string;
+  mainText: string;
   buttonText?: string;
 }
 
 export default function ErrorSorry({
-  redirectUrl = '/main/ALL',
-  mainText = '요청한 페이지를 찾을 수 없어요.',
-  buttonText = '홈으로 돌아가기',
+  redirectUrl,
+  mainText,
+  buttonText,
 }: ErrorProps) {
+  const hasRedirectButton = redirectUrl && buttonText;
+
   return (
     <div className="mt-36 flex flex-col items-center px-20">
       <SorryIcon />
       <div className="text-main1 mt-3 text-4xl font-bold">Sorry...</div>
       <div className="text-main1 text-lg font-bold">{mainText}</div>
-      <div className="mt-10 w-full">
-        <Link
-          href={redirectUrl}
-          className="block w-full rounded-[7px] bg-[#1E1C1C] py-3 text-white"
-        >
-          <span className="flex items-center justify-center gap-1">
-            {buttonText}
-          </span>
-        </Link>
-      </div>
+      {hasRedirectButton && (
+        <div className="mt-10 w-full">
+          <Link
+            href={redirectUrl}
+            className="block w-full rounded-[7px] bg-[#1E1C1C] py-3 text-white"
+          >
+            <span className="flex items-center justify-center gap-1">
+              {buttonText}
+            </span>
+          </Link>
+        </div>
+      )}{' '}
     </div>
   );
 }
