b2e14730757ea213ca37d830bc4f85d730522817
순민 <tkksm2@gmail.com>
2023-08-02 23:36:58 +0900

    fix: redirect bug

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,20 +1,5 @@
-'use client';
-
 import NotFoundPage from '@/components/NotFoundPage';
-import Error from 'next/error';
-import { redirect, usePathname } from 'next/navigation';
 
 export default function NotFound() {
-  const segmentList = usePathname().split('/');
-
-  switch (true) {
-    case segmentList.includes('main'):
-      return redirect('/main/ALL');
-    case segmentList.includes('hottrend'):
-      return redirect('/main/ALL');
-    case segmentList.includes('event'):
-      return redirect('/event/ALL');
-    default:
-      return <NotFoundPage />;
-  }
+  return <NotFoundPage />;
 }
