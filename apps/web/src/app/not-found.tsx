c439d9819066f2a39862dca8ccac3e78b97baad0
SoonMin <89559826+Ssoon-m@users.noreply.github.com>
2023-08-05 23:13:45 +0900

    style(web): 404에러페이지 작업 (#48)
    * style: error page
    
    * fix: redirect bug
    
    * fix: remove use client of NotFoundPage
    
    * fix: change link href of HomeIconButton

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,19 +1,5 @@
-'use client';
-
-import Error from 'next/error';
-import { redirect, usePathname } from 'next/navigation';
+import NotFoundPage from '@/components/NotFoundPage';
 
 export default function NotFound() {
-  const segmentList = usePathname().split('/');
-
-  switch (true) {
-    case segmentList.includes('main'):
-      return redirect('/main/ALL');
-    case segmentList.includes('hottrend'):
-      return redirect('/main/ALL');
-    case segmentList.includes('event'):
-      return redirect('/event/ALL');
-    default:
-      return <Error statusCode={404} />;
-  }
+  return <NotFoundPage />;
 }
